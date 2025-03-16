6cd8f9462025322a363caee40d81a90b76794b14
INDDRSINGH <141555972+INDDRSINGH@users.noreply.github.com>
2025-03-16 19:42:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+from nltk.stem import PorterStemmer
+import re
+
+
+model = pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('tfidf.pkl','rb'))
+
+ps = PorterStemmer()
+
+def stemming(text):
+    lst = []
+    text = re.sub(r'[^a-zA-Z0-9 ]'," ",text)
+    for i in text.split():
+        lst.append(ps.stem(i))
+    return " ".join(lst)
+
+st.title('Spam Classifier')
+
+text = st.text_area("Enter the message below")
+final = ""
+if st.button("Predict"):
+    transform = stemming(text)
+    vector = tfidf.transform([transform])
+    result = model.predict(vector)
+    if result == 1:
+        st.header('Spam')
+    else:
+        st.header('Not Spam')
+
+
+
+
